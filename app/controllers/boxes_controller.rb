74c4e5883e6639fbb643222cd4194cfa2db747d3
Greta Anderson <Greta@Gretas-MacBook-Pro.local>
2016-03-02 17:09:49 -0500

    migrate pickup location to boxes

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -8,7 +8,7 @@ class BoxesController < ApplicationController
 
     @search = Box.search(params[:q])
     @boxes = @search.result
-    
+
   end
 
   # GET /boxes/1
@@ -74,6 +74,6 @@ class BoxesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def box_params
-      params.require(:box).permit(:name, :contents, :description, :price, :length_of_time, :start_date, :end_date, :farm_id)
+      params.require(:box).permit(:name, :contents, :description, :price, :length_of_time, :start_date, :end_date, :farm_id, :pickup_location)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228233052) do
+ActiveRecord::Schema.define(version: 20160302215742) do
 
   create_table "boxes", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20160228233052) do
     t.date     "start_date"
     t.date     "end_date"
     t.integer  "farm_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "size"
     t.float    "weight"
+    t.text     "pickup_location"
   end
 
   create_table "farms", force: :cascade do |t|
