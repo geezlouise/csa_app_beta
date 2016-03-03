3ecb5ae739d8907a452e64e959cca78035a26892
Greta Anderson <Greta@Gretas-MacBook-Pro.local>
2016-03-03 04:15:02 -0500

    work on styling box-listing and other edits -- bug in database when adding to favorites

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,10 +3,10 @@ class CartController < ApplicationController
   def add_to_cart
   	line_item = LineItem.new
   	line_item.box_id = params[:box_id]
-  	line_item.quantity = params[:quantity]
-  	line_item.save
+  	# line_item.quantity = params[:quantity]
+  	# line_item.save
 
-  	line_item.line_item_total = line_item.box.price * line_item.quantity
+  	# line_item.line_item_total = line_item.box.price * line_item.quantity
   	line_item.save
 
   	redirect_to root_path
